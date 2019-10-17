2e425544ea3c4dacbc28676c274ccefa45599403
Lee Tzilantonis <Lee.Tzilantonis@ibm.com>
2019-10-17 11:44:12 +1100

    Removed unused imports

--- tests/test_ev3devices.py
+++ tests/test_ev3devices.py
@@ -1,12 +1,7 @@
 import pytest
 
-from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction)
 
 class TestMotor:
 
